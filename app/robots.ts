70e27457d185a2f38a0cca77928260e71ac52076
Ansh Agrawal <anshdoesanalytics@gmail.com>
2026-06-15 17:48:29 +0530

    Changes

--- app/robots.ts
+++ app/robots.ts
@@ -8,7 +8,7 @@ export default function robots(): MetadataRoute.Robots {
       disallow: ['/api/'],
     },
     sitemap: [
-      'https://joindatalyze.com/sitemap.xml',
+      'https://www.joindatalyze.com/sitemap.xml',
       'https://www.joindatalyze.com/blog/sitemap.xml',
     ],
   };
